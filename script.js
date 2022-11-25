e940243795d774c30ed289360334d43d24f35350
candradwicahyo <candradwicahyo18@gmail.com>
2022-11-25 11:17:50 +0700

    second commit

--- script.js
+++ script.js
@@ -1,81 +1,120 @@
 window.addEventListener('DOMContentLoaded', () => {
   
+  const vConsole = new VConsole();
+  
   const textarea = document.querySelector('.textarea');
   const button = document.querySelector('.button');
-  const allInput = document.querySelectorAll('.input');
-  allInput.forEach(input => {
+  const inputs = document.querySelectorAll('.input');
+  inputs.forEach(input => {
     input.addEventListener('keyup', () => {
-      const data = value();
+      const data = allInputValue();
       stylingButton(data);
       textarea.value = setValue(data);
     });
   });
   
-  function value() {
+  function allInputValue() {
     let text = document.querySelector('#text').value.trim();
     let fontColor = document.querySelector('#font-color').value.trim();
-    let border = document.querySelector('#border').value.trim();
+    let fontWeight = document.querySelector('#font-weight').value.trim();
     let fontSize = document.querySelector('#font-size').value.trim();
+    let border = document.querySelector('#border').value.trim();
     let borderRadius = document.querySelector('#border-radius').value.trim();
-    let paddingTop = document.querySelector('#pt').value.trim();
-    let paddingRight = document.querySelector('#pr').value.trim();
-    let paddingBottom = document.querySelector('#pb').value.trim();
-    let paddingLeft = document.querySelector('#pl').value.trim();
-    let bg = document.querySelector('#bg').value.trim();
-    
-    let data = {
+    let pt = document.querySelector('#pt').value.trim();
+    let pr = document.querySelector('#pr').value.trim();
+    let pb = document.querySelector('#pb').value.trim();
+    let pl = document.querySelector('#pl').value.trim();
+    let bsColor = document.querySelector('#bs-color').value.trim();
+    let hor = document.querySelector('#hor').value.trim();
+    let ver = document.querySelector('#ver').value.trim();
+    let blur = document.querySelector('#blur').value.trim();
+    let spread = document.querySelector('#spread').value.trim();
+    let bgColor = document.querySelector('#bg-color').value.trim();
+    return {
       text: text,
       fontColor: fontColor,
-      border: border,
+      fontWeight: fontWeight,
       fontSize: fontSize,
+      border: border,
       borderRadius: borderRadius,
-      pt: paddingTop,
-      pr: paddingRight,
-      pb: paddingBottom,
-      pl: paddingLeft,
-      bg: bg
+      pt: pt,
+      pr: pr,
+      pb: pb,
+      pl: pl,
+      bsColor: bsColor,
+      hor: hor,
+      ver: ver,
+      blur: blur,
+      spread: spread,
+      bgColor: bgColor
     }
-    
-    return data;
   }
   
+  const inputFontColor = document.querySelector('#font-color');
+  const inputColor = document.querySelector('#input-color');
+  inputColor.addEventListener('change', function() {
+    inputFontColor.value = this.value;
+    button.style.color = this.value;
+    const data = allInputValue();
+    textarea.value = setValue(data);
+  });
+  
+  inputFontColor.addEventListener('keyup', function() {
+    const value = this.value.trim();
+    inputColor.value = this.value.trim();
+  });
+  
   function stylingButton(data) {
-    button.textContent = data.text == '' ? 'click here' : data.text;
-    button.style.color = data.fontColor == '' ? 'black' : data.fontColor;
-    button.style.fontSize = data.fontSize == '' ? '13px' : `${data.fontSize}px`;
-    button.style.border = data.border == '' ? 'none' : data.border;
-    button.style.borderRadius = data.borderRadius == '' ? '0' : `${data.borderRadius}px`;
+    button.textContent = !data.text ? 'click here' : data.text;
+    button.style.color = !data.fontColor ? '#000' : data.fontColor;
+    button.style.fontSize = !data.fontSize ? '13px' : `${data.fontSize}px`;
+    button.style.fontWeight = !data.fontWeight ? '400' : `${data.fontWeight}`;
+    button.style.border = !data.border ? 'none' : `${data.border}px solid black`;
+    button.style.borderRadius = !data.borderRadius ? '0' : `${data.borderRadius}px`;
     button.style.padding = `${data.pt}px ${data.pr}px ${data.pb}px ${data.pl}px`;
-    button.style.background = data.bg == '' ? 'silver' : data.bg;
+    button.style.boxShadow = `${data.hor}px ${data.ver}px ${data.blur}px ${data.spread}px ${data.bsColor}`;
+    button.style.background = !data.bgColor ? 'none' : data.bgColor;
+  }
+  
+  const inputBackground = document.querySelector('#bg-color');
+  const generate = document.querySelector('.generate');
+  generate.addEventListener('click', () => {
+    inputBackground.value = randomHexColor();
+    button.style.background = randomHexColor();
+    const data = allInputValue();
+    textarea.value = setValue(data);
+  });
+  
+  function randomHexColor() {
+    let str = 'abcdef0123456789';
+    let result = '#';
+    for (let i = 0; i < 6; i++) {
+      result += str[Math.floor(Math.random() * str.length)];
+    }
+    return result;
   }
   
   function setValue(data) {
     return `
-    button {
-      color: ${data.fontColor == '' ? 'black' : data.fontColor};
-      font-size: ${data.fontSize == '' ? '13px' : `${data.fontSize}px`};
-      border: ${data.border == '' ? 'none' : data.border};
-      border-radius: ${data.borderRadius == '' ? '0' : `${data.borderRadius}px`};
+    .button {
+      color: ${!data.fontColor ? '#000' : data.fontColor};
+      font-size: ${!data.fontSize ? '13px' : `${data.fontSize}px`};
+      font-weight: ${!data.fontWeight ? '400' : data.fontWeight};
+      border: ${!data.border ? 'none' : `${data.border}px solid black`};
+      border-radius: ${!data.borderRadius ? '0' : `${data.borderRadius}px`};
       padding: ${`${data.pt}px ${data.pr}px ${data.pb}px ${data.pl}px`};
-      background: ${data.bg == '' ? 'silver' : data.bg};
-    }`;
-  }
-  
-  const box = document.querySelector('.box-color');
-  
-  window.addEventListener('keyup', (event) => {
-    if (event.target.classList.contains('font-color')) {
-      const value = event.target.value.trim();
-      box.style.background = (!value) ? '#000' : value;
+      box-shadow: ${`${data.hor}px ${data.ver}px ${data.blur}px ${data.spread}px ${data.bsColor}`}
+      background: ${!data.bgColor ? '#f3f3f3' : data.bgColor};
     }
-  });
+    `;
+  }
   
   const copyButton = document.querySelector('.copy');
   copyButton.addEventListener('click', () => {
     if (textarea.value) {
       textarea.select();
       document.execCommand('copy');
-      alert('berhasil disalin!');
+      alert('code has been copied!');
     }
   });
   
